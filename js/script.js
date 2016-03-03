b1229b318c8c4851811e8bebb34f2cd19f2dac29
Justin Richard <montypython098@gmail.com>
2016-03-02 21:58:56 -0500

    HTML layout finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by Justin_NZXT on 3/2/2016.
  */
+
+
+var $modal = $('#modal');
+
+$.ajax('/url')
+    .done(function(resp){
+        $modal.html(resp.html).foundation('open');
+    });
